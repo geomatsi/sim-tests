5a530a299f222b12f8f927f29b8f28637bd40592
Sergey Matyukevich <geomatsi@gmail.com>
2024-10-21 22:54:07 +0300

    test3: use ACLINT header
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- riscv/src/test3.c
+++ riscv/src/test3.c
@@ -2,15 +2,10 @@
 
 #include "riscv_encoding.h"
 #include "xprintf.h"
+#include "aclint.h"
 #include "htif.h"
 
-#define CLINT_BASE        (0x02000000UL)
-#define REG_MSWI0         ((volatile unsigned long*)(CLINT_BASE + 0x00000000UL))
-#define REG_MTIME         ((volatile unsigned long*)(CLINT_BASE + 0x0000bff8UL))
-#define REG_MTIMECMP      ((volatile unsigned long*)(CLINT_BASE + 0x00004000UL))
-
 void trap_handler(void) __attribute__((interrupt("machine")));
-
 void trap_handler(void)
 {
 	unsigned long mcause, mtval, mepc;
